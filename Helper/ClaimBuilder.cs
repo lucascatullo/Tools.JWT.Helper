f91c156d133349e2482b4dd6c77721207500b2c5
LucasC <lucas@mymarketplacebuilder.com>
2024-07-18 20:02:55 +0200

    claim builder code added

--- Helper/ClaimBuilder.cs
+++ Helper/ClaimBuilder.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 
 namespace Tools.JWT.Helper.Helper;
@@ -11,28 +12,35 @@ internal class ClaimBuilder
 
     public ClaimBuilder AddEmail(string email)
     {
+        _claims.Add(new(JwtRegisteredClaimNames.Sub, email));
         return this;
     }
 
 
     public ClaimBuilder AddGeneratedJti()
     {
+        _claims.Add(new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
         return this;
     }
 
     public ClaimBuilder AddNameIdentifier(string id)
     {
+        _claims.Add(new(ClaimTypes.NameIdentifier, id));
         return this;
     } 
 
 
     public ClaimBuilder AddRoles(string[] roles)
     {
+        foreach (var role in roles)
+            _claims.Add(new Claim(ClaimTypes.Role, role));
         return this; 
     }
 
     public ClaimBuilder AddMetaData(IDictionary<string, string> metadata)
     {
+        foreach (var data in metadata) _claims.Add(new Claim(data.Key, data.Value, "metadata"));
+
         return this;
     }
 
